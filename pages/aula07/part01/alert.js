4eac34f7891e3bc8b4da192057629c7bec0c2c31
Gustavo Ferreira <gfmb94@gmail.com>
2024-09-20 16:54:06 -0300

    introdução a alert, confirm e prompt

---
+++ pages/aula07/part01/alert.js
@@ -0,0 +1,4 @@
+window.alert("Minha primeira mensagem!");
+window.confirm("Está gostando do curso?");
+let res = window.prompt("Qual é o seu nome");
+document.getElementsByTagName("p").innerHTML = res;
